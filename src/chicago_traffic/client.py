d0683e4d790962425d9d127ba790767c512ef146
LukasAnell <lukasmanell@gmail.com>
2026-06-09 15:39:05 +0200

    Rename session to client & Start on implementing get_live_speeds()

--- src/chicago_traffic/client.py
+++ src/chicago_traffic/client.py
@@ -1,16 +1,27 @@
 from httpx import Client
 
+from chicago_traffic.models import TrafficSegment
+
 
 class TrafficClient:
-    base_url: str = "https://data.cityofchicago.org/resource/n4j6-wkkf.json"
+    base_url: str = "https://data.cityofchicago.org/resource"
+    dataset_id: str = "/n4j6-wkkf.json"
 
     app_token: str | None
-    session: Client
+    client: Client
 
     def __init__(self, app_token: str | None = None):
         self.app_token = app_token
-        self.session = Client(base_url=self.base_url)
+        self.client = Client(base_url=self.base_url)
 
         # if user supplied a token, use it in future HTTP requests
         if self.app_token is not None:
-            self.session.headers["X-App-Token"] = self.app_token
+            self.client.headers["X-App-Token"] = self.app_token
+
+    def get_live_speeds(self) -> list[TrafficSegment]:
+        # get response from API at base_url
+        response = self.client.get(self.dataset_id)
+
+        #
+
+        return list()
