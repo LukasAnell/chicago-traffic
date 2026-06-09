c4c34e1002ac88263addf9d011d0e3168e0fe605
LukasAnell <lukasmanell@gmail.com>
2026-06-09 18:59:06 +0200

    Working on parsing JSON response into a TrafficSegment object

--- src/chicago_traffic/client.py
+++ src/chicago_traffic/client.py
@@ -22,6 +22,27 @@ class TrafficClient:
         # get response from API at base_url
         response = self.client.get(self.dataset_id)
 
-        #
+        # parse response into list of TrafficSegment objects
+
+        # turn raw response into structured JSON
+        json_response = response.json()
+
+        # for each item in the JSON response, create a TrafficSegment object and add it to the list of segments
+        segments = []
+        for item in json_response:
+            segment_id: int = item["segment_id"]
+            street: str = item["street"]
+            direction: str = item["_direction"]
+            from_street: str = item["_fromst"]
+            to_street: str = item["tost"]
+            length: float = item["_length"]
+            street_heading: str = item["_strheading"]
+            start_lon: float = item["_comments"]
+            start_lat: float = item["start_lon"]
+            end_lon: float = item["_lif_lat"]
+            end_lat: float = item["_lif_lon"]
+            current_speed: float = item["_lit_lat"]
+            last_updated: str = item["_traffic"]
+            comments: str = item["_last_updt"]
 
         return list()
