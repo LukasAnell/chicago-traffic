e4f34968ad5fceb6793f4911305ae885eb6bc889
Lukas Anell <135393282+LukasAnell@users.noreply.github.com>
2026-06-09 15:07:13 +0200

    Implement basic data model with API response fields, along with custom type for error handling (#1)
    * Re-initialize uv project
    
    * Dataclass for a TrafficSegment
    
    * Custom Exception for package and Added missing fields
    
    * Fix datatypes
    
    * Add path to original Exception in TrafficAPIError
    
    * Fixed data types of fields after some testing
    
    * Remove unnecessary __init__ and revert last_updated datatype to str
    
    * Remove unnecessary __init__ and revert last_updated datatype to str

---
+++ src/chicago_traffic/models.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TrafficSegment:
+    """Represents a traffic segment in the Chicago traffic dataset."""
+
+    segment_id: int | None
+    street: str | None
+    direction: str | None
+    from_street: str | None
+    to_street: str | None
+    length: float | None
+    street_heading: str | None
+    start_lon: float | None
+    start_lat: float | None
+    end_lon: float | None
+    end_lat: float | None
+    current_speed: float | None
+    last_updated: str | None
+    comments: str | None
+
+
+class TrafficAPIError(Exception):
+    """Custom exception for errors related to the Traffic API."""
+
+    cause: Exception | None
+
+    def __init__(self, message: str, cause: Exception | None = None):
+        super().__init__(message)
+
+        self.cause = cause
