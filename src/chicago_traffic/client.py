9159e641fc5c59220ef305ffdbad4141cad4c3eb
LukasAnell <lukasmanell@gmail.com>
2026-06-10 20:46:04 +0200

    Added inner try-except to just skip fields that aren't included in response

--- src/chicago_traffic/client.py
+++ src/chicago_traffic/client.py
@@ -1,3 +1,4 @@
+import warnings
 from datetime import datetime
 from types import TracebackType
 from typing import Callable, cast
@@ -82,43 +83,50 @@ class TrafficClient:
             # for each item in the JSON response, create a TrafficSegment object and add it to the list of segments
             segments: list[TrafficSegment] = []
             for item in json_response:
-                segment_id: int = self._get_required(item, "segment_id", int)
-                street: str = self._get_required(item, "street", str)
-                direction: str = self._get_required(item, "direction", str)
-                from_street: str = self._get_required(item, "_fromst", str)
-                to_street: str = self._get_required(item, "_tost", str)
-                length: float = self._get_required(item, "_length", float)
-                street_heading: str = self._get_required(item, "_strheading", str)
-                comments: str | None = item.get("_comments")
-                start_lon: float = self._get_required(item, "start_lon", float)
-                start_lat: float = self._get_required(item, "_lif_lat", float)
-                end_lon: float = self._get_required(item, "_lit_lon", float)
-                end_lat: float = self._get_required(item, "_lit_lat", float)
-                current_speed: float = self._get_required(item, "_traffic", float)
-                last_updated: datetime = self._get_required(
-                    item,
-                    "_last_updt",
-                    lambda s: datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f"),
-                )
+                try:
+                    segment_id: int = self._get_required(item, "segment_id", int)
+                    street: str = self._get_required(item, "street", str)
+                    direction: str = self._get_required(item, "direction", str)
+                    from_street: str = self._get_required(item, "_fromst", str)
+                    to_street: str = self._get_required(item, "_tost", str)
+                    length: float = self._get_required(item, "_length", float)
+                    street_heading: str = self._get_required(item, "_strheading", str)
+                    comments: str | None = item.get("_comments")
+                    start_lon: float = self._get_required(item, "start_lon", float)
+                    start_lat: float = self._get_required(item, "_lif_lat", float)
+                    end_lon: float = self._get_required(item, "_lit_lon", float)
+                    end_lat: float = self._get_required(item, "_lit_lat", float)
+                    current_speed: float = self._get_required(item, "_traffic", float)
+                    last_updated: datetime = self._get_required(
+                        item,
+                        "_last_updt",
+                        lambda s: datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f"),
+                    )
 
-                segment: TrafficSegment = TrafficSegment(
-                    segment_id,
-                    street,
-                    direction,
-                    from_street,
-                    to_street,
-                    length,
-                    street_heading,
-                    comments,
-                    start_lon,
-                    start_lat,
-                    end_lon,
-                    end_lat,
-                    current_speed,
-                    last_updated,
-                )
+                    segment: TrafficSegment = TrafficSegment(
+                        segment_id,
+                        street,
+                        direction,
+                        from_street,
+                        to_street,
+                        length,
+                        street_heading,
+                        comments,
+                        start_lon,
+                        start_lat,
+                        end_lon,
+                        end_lat,
+                        current_speed,
+                        last_updated,
+                    )
 
-                segments.append(segment)
+                    segments.append(segment)
+                except TrafficAPIError as e:
+                    warnings.warn(
+                        f"Skipping segment due to error: {e}",
+                        category=RuntimeWarning,
+                    )
+                    continue
 
         except TrafficAPIError:
             raise
